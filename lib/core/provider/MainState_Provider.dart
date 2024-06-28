13064f1bafa90da1bcc74a783ebccff328ab60b6
Raghavan <raghavan.work@outlook.com>
2024-06-29 00:14:13 +0530

    added Tech_news

--- lib/core/provider/MainState_Provider.dart
+++ lib/core/provider/MainState_Provider.dart
@@ -19,6 +19,15 @@ class MainstateProvider extends ChangeNotifier {
     const SearchScreen(),
     const ProfileScreen()
   ];
+  List<String> newtemp = [
+    "OpenAI Develops CriticGPT Model Capable of Spotting GPT-4 Code Generation Errors",
+    "Bolivia Reverses Bitcoin Ban, Legalises Crypto Transactions for Banks",
+    "Character.AI Introduces Character Calls Feature That Lets Users 'Call' AI Chatbots	",
+    "CMF Phone 1 Specifications Revealed Ahead of Launch on July 8; to Feature MediaTek Dimensity 7300 Chipset",
+    "Truecaller Fraud Insurance Launched in India for iOS and Android Users",
+    "Oppo Reno 12F 5G With MediaTek Dimensity 6300 SoC, 5,000mAh Battery Unveiled: Specifications",
+    "RBI Releases Financial Stability Report With Mention of DeFi, US Efforts to Regulate Crypto Sector"
+  ];
 
   void updatenavstate(int index) {
     currentnavtab = index;
--- lib/pages/shared_widgets/TechNews_Widget.dart
+++ lib/pages/shared_widgets/TechNews_Widget.dart
@@ -1,43 +1,95 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:freeskills/core/provider/MainState_Provider.dart';
+import 'package:provider/provider.dart';
 
 class TechnewsWidget extends StatelessWidget {
   const TechnewsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(9.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Tech News",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w400,
-                fontSize: 17.sp),
-          ),
-          SizedBox(height: 4.h),
-          SizedBox(
-            width: double.infinity,
-            height: 0.12.sh,
-            child: Container(
-                decoration: BoxDecoration(
-              color: const Color.fromRGBO(90, 90, 90, 100),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2), // Shadow color
-                  spreadRadius: 3, // Spread radius
-                  blurRadius: 5, // Blur radius
-                  offset: Offset(0, 3), // Shadow offset (x, y)
+    return Consumer<MainstateProvider>(
+      builder: (BuildContext context, MainstateProvider value, Widget? child) {
+        return Padding(
+          padding: const EdgeInsets.all(9.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Tech News",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 17.sp),
+              ),
+              SizedBox(height: 4.h),
+              SizedBox(
+                width: double.infinity,
+                height: 0.13.sh,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(90, 90, 90, 100),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2), // Shadow color
+                        spreadRadius: 3, // Spread radius
+                        blurRadius: 5, // Blur radius
+                        offset: Offset(0, 3), // Shadow offset (x, y)
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(12.5),
+                  ),
+                  child: CarouselSlider.builder(
+                    itemCount: value.newtemp.length,
+                    itemBuilder: (BuildContext context, int itemIndex,
+                            int pageViewIndex) =>
+                        Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              value.newtemp[itemIndex],
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.sp,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Spacer(),
+                            Row(
+                              children: [
+                                Text(
+                                  "Source:google.com",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10.sp,
+                                      fontWeight: FontWeight.w200),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "Date:69/69/6969",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10.sp,
+                                      fontWeight: FontWeight.w200),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    options:
+                        CarouselOptions(viewportFraction: 1, autoPlay: true),
+                  ),
                 ),
-              ],
-              borderRadius: BorderRadius.circular(25),
-            )),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
